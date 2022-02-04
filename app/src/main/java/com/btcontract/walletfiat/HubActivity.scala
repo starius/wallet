e40dc9385e551c2bc098c96dfe2bf6d56de0a7c1
Anton Gushcha <ncrashed@protonmail.com>
2022-02-04 16:50:13 +0300

    Change svg icon color and autoconnect mainnet wallet to FC node

--- app/src/main/java/com/btcontract/walletfiat/HubActivity.scala
+++ app/src/main/java/com/btcontract/walletfiat/HubActivity.scala
@@ -75,7 +75,7 @@ object HubActivity {
 
   def requestHostedChannel: Unit = {
     val localParams = LNParams.makeChannelParams(isFunder = false, LNParams.minChanDustLimit)
-    new HCOpenHandler(LNParams.syncParams.motherbase, randomBytes32, localParams.defaultFinalScriptPubKey, LNParams.cm) {
+    new HCOpenHandler(LNParams.syncParams.ergveinFiat, randomBytes32, localParams.defaultFinalScriptPubKey, LNParams.cm) {
       // Stop automatic HC opening attempts on getting any kind of local/remote error, this won't be triggered on disconnect
       def onFailure(reason: Throwable): Unit = WalletApp.app.prefs.edit.putBoolean(WalletApp.OPEN_HC, false).commit
       def onEstablished(cs: Commitments, channel: ChannelHosted): Unit = implant(cs, channel)
