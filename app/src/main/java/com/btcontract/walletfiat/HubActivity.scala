324bff568f319faec6148b5daf0d672d5b5d9e3e
Anton Gushcha <ncrashed@protonmail.com>
2022-02-05 14:07:13 +0300

    Use ergvein node as default for Fiat channels and fix translation to Russian

--- app/src/main/java/com/btcontract/walletfiat/HubActivity.scala
+++ app/src/main/java/com/btcontract/walletfiat/HubActivity.scala
@@ -74,7 +74,7 @@ object HubActivity {
 
   def requestHostedChannel: Unit = {
     val localParams = LNParams.makeChannelParams(isFunder = false, LNParams.minChanDustLimit)
-    new HCOpenHandler(LNParams.syncParams.ergveinFiat, randomBytes32, localParams.defaultFinalScriptPubKey, LNParams.cm) {
+    new HCOpenHandler(LNParams.syncParams.ergveinNet, randomBytes32, localParams.defaultFinalScriptPubKey, LNParams.cm) {
       // Stop automatic HC opening attempts on getting any kind of local/remote error, this won't be triggered on disconnect
       def onFailure(reason: Throwable): Unit = WalletApp.app.prefs.edit.putBoolean(WalletApp.OPEN_HC, false).commit
       def onEstablished(cs: Commitments, channel: ChannelHosted): Unit = implant(cs, channel)
