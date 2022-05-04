1b64897821740358913b90a7901b23287efb45da
Anton Gushcha <ncrashed@protonmail.com>
2022-05-05 00:30:13 +0200

    Tag new version 2.4.22

--- sign-apk.sh
+++ sign-apk.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env nix-shell
 #!nix-shell -i bash -p androidsdk_9_0 -p apksigner
-SDK_DERIVATION=/nix/store/wzxdnnl0kkc4n8gkx8z3lx9fjdcsqxxm-androidsdk
-rm app/build/outputs/apk/release/StandardSats-SBW-2.4.21-aligned.apk || true
-$SDK_DERIVATION/libexec/android-sdk/build-tools/31.0.0/zipalign -v 4 app/build/outputs/apk/release/StandardSats-SBW-2.4.21.apk app/build/outputs/apk/release/StandardSats-SBW-2.4.21-aligned.apk
-apksigner sign --ks sbw.keystore --ks-key-alias sbw --v1-signing-enabled true --v2-signing-enabled true app/build/outputs/apk/release/StandardSats-SBW-2.4.21-aligned.apk
+set -xe
+echo $PATH
+SDK_DERIVATION=/nix/store/w1259lpbs1wg39ji71hbyvvq7q8d3ac4-build-tools-31.0.0
+rm app/build/outputs/apk/release/StandardSats-SBW-2.4.22-aligned.apk || true
+$SDK_DERIVATION/libexec/android-sdk/build-tools/31.0.0/zipalign -v 4 app/build/outputs/apk/release/StandardSats-SBW-2.4.22.apk app/build/outputs/apk/release/StandardSats-SBW-2.4.22-aligned.apk
+apksigner sign --ks sbw.keystore --ks-key-alias sbw --v1-signing-enabled true --v2-signing-enabled true app/build/outputs/apk/release/StandardSats-SBW-2.4.22-aligned.apk
