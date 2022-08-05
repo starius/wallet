dc164cd63ea8319cde87d92e158c627d2f713bfb
Anton Gushcha <ncrashed@protonmail.com>
2022-08-06 01:29:34 +0700

    Add French Frank, Deutsche Mark, Salvadoran colon, pesso, dollar

--- app/src/main/java/immortan/utils/FiatRates.scala
+++ app/src/main/java/immortan/utils/FiatRates.scala
@@ -13,12 +13,14 @@ object FiatRates {
 }
 
 class FiatRates(bag: DataBag) extends CanBeShutDown {
-  val customFiatSymbols: Map[String, String] = Map("rub" -> "\u20BD", "usd" -> "$", "inr" -> "₹", "gbp" -> "£", "cny" -> "CN¥", "jpy" -> "¥", "brl" -> "R$", "eur" -> "€", "krw" -> "₩", "cym" -> "￠", "lvl" -> "ℒ\uD835\uDCC8")
+  val customFiatSymbols: Map[String, String] = Map("rub" -> "\u20BD", "usd" -> "$", "inr" -> "₹", "gbp" -> "£", "cny" -> "CN¥", "jpy" -> "¥", "brl" -> "R$", "eur" -> "€", "krw" -> "₩",
+    "cym" -> "￠", "lvl" -> "ℒ\uD835\uDCC8", "svc" -> "₡", "frf" -> "₣")
 
   val universallySupportedSymbols: Map[String, String] = Map("usd" -> "US Dollar", "eur" -> "Euro", "jpy" -> "Japanese Yen", "cny" -> "Chinese Yuan", "inr" -> "Indian Rupee", "cad" -> "Canadian Dollar",
     "rub" -> "Русский Рубль", "brl" -> "Real Brasileiro", "czk" -> "Česká Koruna", "gbp" -> "Pound Sterling", "aud" -> "Australian Dollar", "try" -> "Turkish Lira", "nzd" -> "New Zealand Dollar",
     "thb" -> "Thai Baht", "twd" -> "New Taiwan Dollar", "krw" -> "South Korean won", "clp" -> "Chilean Peso", "sgd" -> "Singapore Dollar", "hkd" -> "Hong Kong Dollar", "pln" -> "Polish złoty",
-    "dkk" -> "Danish Krone", "sek" -> "Swedish Krona", "chf" -> "Swiss franc", "huf" -> "Hungarian forint", "cym" -> "Welsh Pound", "lvl" -> "Latvian lat")
+    "dkk" -> "Danish Krone", "sek" -> "Swedish Krona", "chf" -> "Swiss franc", "huf" -> "Hungarian forint", "cym" -> "Welsh Pound", "lvl" -> "Latvian lat", "dm" -> "Deutsche Mark",
+    "frf" -> "French franc", "svc" -> "Salvadoran colón", "esd" -> "Salvadoran dollar", "sps" -> "Salvadoran peso")
 
   def reloadData: Tools.Fiat2Btc = fr.acinq.eclair.secureRandom nextInt 3 match {
     case 0 => to[CoinGecko](Tools.get("https://api.coingecko.com/api/v3/exchange_rates").string).rates.map { case (code, item) => code.toLowerCase -> item.value }
@@ -26,10 +28,23 @@ class FiatRates(bag: DataBag) extends CanBeShutDown {
     case _ => to[Bitpay](Tools.get("https://bitpay.com/rates").string).data.map { case BitpayItem(code, rate) => code.toLowerCase -> rate }.toMap
   }
 
-  def enrichFiats(fs: Tools.Fiat2Btc): Tools.Fiat2Btc = fs ++ Map(
-    "cym" -> fs.get("eur").getOrElse(0.0),
-    "lvl" -> 1.423 * fs.get("eur").getOrElse(0.0) // Last known exchange rate
-  )
+  def enrichFiats(fs: Tools.Fiat2Btc): Tools.Fiat2Btc = {
+    val eur = fs.get("eur").getOrElse(0.0)
+    val usd = fs.get("usd").getOrElse(0.0)
+    val frf = (1.0/6.55957) * eur // Last known exchange rate 17 February 2002
+    val richFiats = Map(
+      "cym" -> eur,
+      "lvl" -> 1.423 * eur, // Last known exchange rate
+      "dm" ->  (1.0/1.95583) * eur, // Last known exchange rate 1 March 2002
+      "frf" -> frf,
+      "esd" -> usd,
+      "svc" -> (1/8.75) * usd, // Last known exchange rate 2001
+      "sps" -> 5 * frf // 1 salvadoran peso = 5 francs 1919
+    )
+    println(s"Rich fiats: $richFiats")
+    fs ++ richFiats
+  }
+
 
   override def becomeShutDown: Unit = {
     subscription.unsubscribe
