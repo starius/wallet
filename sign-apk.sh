e29d4d811be6fb5b2eed3aa8ccd924353d39f6db
Anton Gushcha <ncrashed@protonmail.com>
2022-09-20 16:24:50 +0700

    Release 3.7.0

--- sign-apk.sh
+++ sign-apk.sh
@@ -3,7 +3,7 @@
 set -xe
 echo $PATH
 SDK_DERIVATION=/nix/store/w1259lpbs1wg39ji71hbyvvq7q8d3ac4-build-tools-31.0.0
-VERSION=3.6.0
+VERSION=3.7.0
 rm app/build/outputs/apk/release/StandardSats-SBW-$VERSION-aligned.apk || true
 $SDK_DERIVATION/libexec/android-sdk/build-tools/31.0.0/zipalign -v 4 app/build/outputs/apk/release/StandardSats-SBW-$VERSION.apk app/build/outputs/apk/release/StandardSats-SBW-$VERSION-aligned.apk
 apksigner sign --ks release.keystore --ks-key-alias release --v1-signing-enabled true --v2-signing-enabled true app/build/outputs/apk/release/StandardSats-SBW-$VERSION-aligned.apk
