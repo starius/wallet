94e80e6098cc6b53df5e1cc5a9ff24818a1363c5
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2022-11-05 18:59:54 +0100

    name refactoring

--- app/src/main/java/com/btcontract/walletfiat/SettingsActivity.scala
+++ app/src/main/java/com/btcontract/walletfiat/SettingsActivity.scala
@@ -318,8 +318,8 @@ class SettingsActivity extends BaseActivity with HasTypicalChainFee with ChoiceR
 
       val links = new TitleView("&#9996;")
       addFlowChip(links.flow, getString(manual), R.drawable.border_green, _ => me browse "https://sbw.finance/posts/manual")
-      addFlowChip(links.flow, getString(sources), R.drawable.border_green, _ => me browse "https://github.com/btcontract/wallet")
-      addFlowChip(links.flow, getString(twitter), R.drawable.border_blue, _ => me browse "https://twitter.com/SimpleBtcWallet")
+      addFlowChip(links.flow, getString(sources), R.drawable.border_green, _ => me browse "https://github.com/standardsats/wallet")
+      addFlowChip(links.flow, getString(twitter), R.drawable.border_blue, _ => me browse "https://twitter.com/standard_sats")
       addFlowChip(links.flow, "&#9825; Rate us", R.drawable.border_green, _ => me bringRateDialog null)
 
       for (count <- LNParams.logBag.count if count > 0) {
